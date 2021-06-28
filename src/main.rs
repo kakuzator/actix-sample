7aea1e56d428960778f69ce032249035fb7bb009
kakuzator <wyarbo0461@yahoo.co.jp>
2021-06-28 23:34:23 +0900

    post送信作成途中

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use actix_web::{get , App , HttpResponse , HttpServer};
+use actix_web::{get , post , web , App , HttpResponse , HttpServer};
+use rusqlite::params;
 
 #[get("/")]
 async fn index() -> Result<HttpResponse , actix_web::Error>
 {
     let response_body = "HellorWorld";
     Ok(HttpResponse::Ok().body(response_body))
+}
+
+#[post("/add")]
+async fn add( ){
+    /* let con = db.get()?;
+    conn.execute("INSETR INTO todo (text) VALUES (?)" , $[&params.text])?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION , "/")
+        .finish()) */
+    let data = params.text;
 }
 
 #[actix_rt::main]
